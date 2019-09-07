ee3be754fde1dd094b959d146eb84df1734bbcb5
Jooyeon <wndus85@gmail.com>
2019-09-07 22:29:16 +0900

    답변들 보는 페이지 구현

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,13 +1,13 @@
 class AnswersController < ApplicationController
   def index
-
+    @answers = Answer.all
   end
 
   def create
     @answer = Answer.where(user_id: current_user.id).first_or_create
     @answer.ordering = answer_params[:ordering].split(',').map(&:to_i)
     @answer.save!
-    redirect_to action: index
+    redirect_to action: "index"
   end
 
   def show
