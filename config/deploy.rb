59548d8e194a7d26ad73e5188929304eb4a765fc
Jooyeon <wndus85@gmail.com>
2019-09-08 21:35:39 +0900

    경로 변경

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :passenger_restart_with_touch, true
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/CHAMP_FROGS"
+set :deploy_to, "/home/deploy/webapp/CHAMP_FROGS"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
