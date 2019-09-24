99d98a8d53bb52c55f59b6ab63ba44ac3926dbee
Jooyeon <wndus85@gmail.com>
2019-09-24 22:18:20 +0900

    가장 중요하다고 생각한 카드 보여주는 로직 수정. 가중치를 주어 평균을 냄.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,20 +1,21 @@
 class Answer < ApplicationRecord
   belongs_to :user
 
-
   def self.get_1st
     ordering = Answer.pluck('ordering')
     Rails.logger.info ordering
 
-    first = []
-    ordering.each do |i|
-      first << i.first
-    end
+    result = {1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0, 9 => 0, 10 => 0}
 
-    count = Hash.new(0)
-    first.each { |motivator| count[motivator] += 1 }
-    result = count.sort_by { |k, v| v }
+
+    ordering.each do |key|
+        score = 10
+        key.each do |k|
+          result[k] +=score
+          score -=1
+        end
+    end
+    result = result.sort_by{|_key, value| value}.reverse.to_h
     result
   end
-
 end
